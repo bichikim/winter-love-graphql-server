37a1d3064e0c6f91cabe3a252bd5e55d37264121
Bichi Kim <bichi@live.co.kr>
2019-09-24 00:03:21 +0900

    init

--- src/mongoose/Users.ts
+++ src/mongoose/Users.ts
@@ -1,10 +1,11 @@
-import {model, Schema} from 'mongoose'
+import {Schema} from 'mongoose'
+
 const {ObjectId} = Schema.Types
 
 export default new Schema<any>({
   id: ObjectId,
   name: {type: String},
-  email: {type: String, unique: true},
+  email: {type: String, unique: true, index: true},
   password: {type: String},
   oauthFrom: {type: String},
   oauthToken: {type: String},
